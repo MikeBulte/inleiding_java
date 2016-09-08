5feee427703f11c781d9db682a59769897a0cae4
Mike <mikebulte@roc-dev.com>
2016-09-08 13:10:42 +0200

    first java applet opdracht 4.2 af

--- src/applet_tekenen/HuisMetDeur.java
+++ src/applet_tekenen/HuisMetDeur.java
@@ -3,12 +3,10 @@ package applet_tekenen;
 /**
  * Created by Mike on 9/8/2016.
  */
-//Voorbeeld 4.1
-
 import java.awt.*;
-        import java.applet.*;
+import java.applet.*;
 
-public class GelijkbenigeDriehoek extends Applet {
+public class HuisMetDeur extends Applet {
 
     public void init() {
     }
@@ -19,5 +17,12 @@ public class GelijkbenigeDriehoek extends Applet {
         g.drawLine(100,200,300,200);
         g.drawLine(300,200,200,050);
         g.drawLine(100,200,200,050);
+        g.drawRect(100,200,200,200);
+        g.drawRect(200,300,80,100);
+        g.drawRect(140,250,50,50);
+        g.drawRect(130,330,50,50);
+        g.drawRect(175,100,50,100);
+        g.drawOval(210,140,10,10);
+        g.drawOval(265,350,10,10);
     }
 }
