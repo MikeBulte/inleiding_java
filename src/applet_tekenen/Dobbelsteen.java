9d927b2e7bfc66a21fb0f99b2dbf258e002a3694
Mike <mikebulte@roc-dev.com>
2016-09-08 19:54:22 +0200

    first java applet 4.7 Dobbelsteen af

---
+++ src/applet_tekenen/Dobbelsteen.java
@@ -0,0 +1,24 @@
+package applet_tekenen;
+
+/**
+ * Created by Mike on 9/8/2016.
+ */
+import java.awt.*;
+import java.applet.*;
+
+public class Dobbelsteen extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+    g.setColor(Color.black);
+    g.fillRoundRect(25,25,100,100,20,20);
+    g.setColor(Color.white);
+    g.fillOval(40,40,20,20);
+    g.fillOval(90,40,20,20);
+    g.fillOval(90,90,20,20);
+    g.fillOval(40,90,20,20);
+
+    }
+}
