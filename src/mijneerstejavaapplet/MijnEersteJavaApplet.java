f447bb8e92e74de1f508185cea44029880063fd4
Mike <mikebulte@roc-dev.com>
2016-09-08 11:22:56 +0200

    first java applet opdracht 2.1

--- src/mijneerstejavaapplet/MijnEersteJavaApplet.java
+++ src/mijneerstejavaapplet/MijnEersteJavaApplet.java
@@ -14,12 +14,13 @@ public class MijnEersteJavaApplet extends Applet {
 
     //een (lege) methode die de Applet initialiseert
     public void init() {
-
+    setBackground(Color.blue);
     }
 
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        g.drawString ("Hello Java",50,60 );
+        g.setColor(Color.yellow);
+        g.drawString ("Mike Bulte",50,60 );
     }
 
 }
