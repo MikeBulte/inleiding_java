ce0e54d4a7b93553c5e83adfe1fd3cee798f18a9
Mike <mikebulte@roc-dev.com>
2016-09-08 11:48:13 +0200

    first java applet opdracht 4.1 niet af

---
+++ src/applet_tekenen/GelijkbenigeDriehoek.java
@@ -0,0 +1,23 @@
+package applet_tekenen;
+
+/**
+ * Created by Mike on 9/8/2016.
+ */
+//Voorbeeld 4.1
+
+import java.awt.*;
+        import java.applet.*;
+
+public class GelijkbenigeDriehoek extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.drawLine(100,200,300,200);
+        g.drawLine(300,200,250,150);
+        g.drawLine(100,200,250,150);
+    }
+}
