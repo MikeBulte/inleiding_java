0c209b81ff09c6b7c19df7e2724e06ff1deb4643
Mike <mikebulte@roc-dev.com>
2016-09-08 13:13:51 +0200

    first java applet opdracht 4.2 met comments

--- src/applet_tekenen/HuisMetDeur.java
+++ src/applet_tekenen/HuisMetDeur.java
@@ -14,15 +14,20 @@ public class HuisMetDeur extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.blue);
         g.setColor(Color.yellow);
-        g.drawLine(100,200,300,200);
-        g.drawLine(300,200,200,050);
-        g.drawLine(100,200,200,050);
-        g.drawRect(100,200,200,200);
-        g.drawRect(200,300,80,100);
-        g.drawRect(140,250,50,50);
-        g.drawRect(130,330,50,50);
-        g.drawRect(175,100,50,100);
-        g.drawOval(210,140,10,10);
-        g.drawOval(265,350,10,10);
+         //dak
+          g.drawLine(100,200,300,200);
+          g.drawLine(300,200,200,050);
+          g.drawLine(100,200,200,050);
+        //ramen
+             g.drawRect(100,200,200,200);
+             g.drawRect(200,300,80,100);
+             g.drawRect(140,250,50,50);
+             g.drawRect(130,330,50,50);
+          //deuren
+                  g.drawRect(180,320,50,50);
+                  g.drawRect(175,100,50,100);
+     //deurknoppen
+                     g.drawOval(210,140,10,10);
+                     g.drawOval(265,350,10,10);
     }
 }
