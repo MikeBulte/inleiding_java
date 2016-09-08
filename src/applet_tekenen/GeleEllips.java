c8da9234c019a59a8fd6d75232250028c14de67b
Mike <mikebulte@roc-dev.com>
2016-09-08 19:35:12 +0200

    first java applet 4.5 Gele Ellips Af

---
+++ src/applet_tekenen/GeleEllips.java
@@ -0,0 +1,24 @@
+package applet_tekenen;
+
+/**
+ * Created by Mike on 9/8/2016.
+ */
+import java.awt.*;
+import java.applet.*;
+
+public class GeleEllips extends Applet {
+
+    public void init() {
+    }
+
+ public void paint(Graphics g) {
+
+     setBackground(Color.BLUE);
+     g.setColor(Color.yellow);
+     g.fillArc(100,100,150,75,360,360);
+
+
+
+
+ }
+}
