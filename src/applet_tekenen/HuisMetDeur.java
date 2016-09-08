0b6ef1c2d59621244be4b018383816b805d69f80
Mike <mikebulte@roc-dev.com>
2016-09-08 16:35:17 +0200

    first java applet alleen is het dak nu niet 24/7 lek

--- src/applet_tekenen/HuisMetDeur.java
+++ src/applet_tekenen/HuisMetDeur.java
@@ -12,22 +12,26 @@ public class HuisMetDeur extends Applet {
     }
 
     public void paint(Graphics g) {
-        setBackground(Color.blue);
-        g.setColor(Color.yellow);
+        setBackground(Color.BLUE);
+        g.setColor(Color.red);
          //dak
-          g.drawLine(100,200,300,200);
-          g.drawLine(300,200,200,050);
-          g.drawLine(100,200,200,050);
+        int[] xPoints = {200,100,300};
+        int[] yPoints = {50,200,200};
+        g.fillPolygon(xPoints, yPoints, 3);
+
+        //huis
+             g.setColor(Color.DARK_GRAY);
+             g.fillRect(100,200,200,200);
+        //deur
+             g.setColor(Color.white);
+             g.fillRect(200,300,50,100);
+             g.setColor(Color.lightGray);
+             g.fillOval(235,350,10,10);
         //ramen
-             g.drawRect(100,200,200,200);
-             g.drawRect(200,300,80,100);
-             g.drawRect(140,250,50,50);
-             g.drawRect(130,330,50,50);
-          //deuren
-                  g.drawRect(180,320,50,50);
-                  g.drawRect(175,100,50,100);
-     //deurknoppen
-                     g.drawOval(210,140,10,10);
-                     g.drawOval(265,350,10,10);
+        g.setColor(Color.cyan);
+             g.fillRect(125,300,50,50);
+             g.fillRect(120,225,70,50);
+             g.fillRect(210,225,70,50);
+
     }
 }
---
+++ src/applet_tekenen/NederlandseVlag.java
@@ -0,0 +1,24 @@
+package applet_tekenen;
+
+/**
+ * Created by Mike on 9/8/2016.
+ */
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class NederlandseVlag extends Applet {
+
+public void init() {
+}
+    public void paint (Graphics g){
+        setBackground(Color.black);
+        g.setColor(Color.RED);
+        g.fillRect(100,100,200,50);
+        g.setColor(Color.white);
+        g.fillRect(100,150,200,50);
+        g.setColor(Color.blue);
+        g.fillRect(100,200,200,50);
+    }
+}
