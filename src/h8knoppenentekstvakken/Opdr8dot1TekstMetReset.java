49371296b29b03db875d84e6965af0fce63ddaaf
Mike <mikebulte@roc-dev.com>
2016-09-22 13:07:56 +0200

    inleiding java h8.1 is af

--- src/h8knoppenentekstvakken/Opdr8dot1TekstMetReset.java
+++ src/h8knoppenentekstvakken/Opdr8dot1TekstMetReset.java
@@ -2,21 +2,56 @@ package h8knoppenentekstvakken;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Created by Mike on 9/20/2016.
+ * Dit is tekstvak met 2 knoppen, 1 hiervan print de tekst op de string eronder terwijl de tweede de string en het
+ * tekstvak reset.
  */
 public class Opdr8dot1TekstMetReset extends Applet {
 
-    Button Knop;
+    private TextField textField;
+    private String stri;
 
     public void init() {
-    Knop = new Button("Klik Hier");
-    add(Knop);
+        //Hier liggen alle knoppen en tekstvelden met hun locale variabelen
+        Button knop = new Button("Ok");
+        knop.addActionListener(new KnopListener());
+        Button reset = new Button("Reset");
+        reset.addActionListener(new ResetListener());
+        Label lab = new Label("Deze tekst reset");
+        textField = new TextField("", 20);
+        add(lab);
+        add(textField);
+        add(knop);
+        add(reset);
+        stri = "hier komt wat je wilt";
+
     }
 
     public void paint(Graphics g) {
-     g.drawString("boep",100,100);
+        g.drawString(stri, 60, 60);
 
+    }
+
+    private class ResetListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            textField.setText(" ");
+            repaint();
+            stri = "Reset";
+        }
+
+
+    }
+
+
+    private class KnopListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            stri = textField.getText();
+            repaint();
+        }
     }
 }
