11cdb90f8431dff8d85324ee4dc259fadfa0057c
Mike <mikebulte@roc-dev.com>
2017-02-01 10:00:19 +0100

    Duurde veel te lang om te leren hoe dit moest

---
+++ src/h12tabellen/Opdr12dot2VijfentwintigKnoppen.java
@@ -0,0 +1,33 @@
+package h12tabellen;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by Mike on 1/10/2017.
+ * Een loop en een array om 25 knoppen te maken
+ */
+public class Opdr12dot2VijfentwintigKnoppen extends Applet {
+
+    @Override
+    public void init() {
+        super.init();
+
+        //Button knoppenArray[];
+        //knoppenArray = new Button[25];
+        Button[] knoppenArray = new Button[25];
+
+        for (int teller = 0; teller < knoppenArray.length; teller++) {
+            //Object krijgt een waarde
+            knoppenArray[teller] = new Button("Knop" + teller);
+            add(knoppenArray[teller]);
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+
+    }
+}
