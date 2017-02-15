fbbac459ef33d84487a58184102c7ecd44766c53
Mike <mikebulte@roc-dev.com>
2017-02-15 08:55:48 +0100

    Initial Commit, maybe make the textfields empty themselves after a value is entered later.

--- src/h12tabellen/Opdr12PraktijkOpdrTabellen.java
+++ src/h12tabellen/Opdr12PraktijkOpdrTabellen.java
@@ -7,20 +7,30 @@ import java.awt.event.ActionListener;
 
 /**
  * Created by Mike on 2/3/2017.
+ * De User kan 2 Strings van tekst of nummers invoeren in de tekstvakken, die worden vervolgens als waarde in een van
+ * 2 Array's geplaatst.
  */
 public class Opdr12PraktijkOpdrTabellen extends Applet {
 
-    private int[] telefoonnummersIntArray;
-
     private String[] namenStringArray;
 
-    private TextField namenTextField = new TextField("", 5);
-    private TextField telefoonnummersTextField = new TextField("", 5);
+    private String[] telefoonnummersStringArray;
 
+    private TextField namenTextField = new TextField("", 8);
+    private TextField telefoonnummersTextField = new TextField("", 12);
+
+    private boolean checkBoolean = false;
+
+    private int teller = 0;
 
     @Override
     public void init() {
         super.init();
+
+        this.setSize(500, 400);
+
+        namenStringArray = new String[10];
+        telefoonnummersStringArray = new String[10];
 
         add(namenTextField);
         namenTextField.addActionListener(new NaamEnTelefoonActionListener());
@@ -37,13 +47,37 @@ public class Opdr12PraktijkOpdrTabellen extends Applet {
     public void paint(Graphics g) {
         super.paint(g);
 
+        int yCord = 60;
 
+        if (checkBoolean) {
+            for (int i = 0; i < 10; i++) {
+                g.drawString("Naam " + i + " is " + namenStringArray[i], 50, yCord);
+                g.drawString("Telefoon nummer " + i + " is " + telefoonnummersStringArray[i], 200, yCord);
+                yCord += 20;
+            }
+        } else {
+            g.drawString("De teller staat nu op " + teller, 50, 60);
+        }
     }
 
     private class NaamEnTelefoonActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
+            String naamInvoerString = namenTextField.getText();
+            String telefoonInvoerString = telefoonnummersTextField.getText();
+
+            namenStringArray[teller] = naamInvoerString;
+            telefoonnummersStringArray[teller] = telefoonInvoerString;
+
+            if (teller < 10)
+                teller++;
+
+            if (teller == 10) {
+                checkBoolean = true;
+                repaint();
+            }
+            repaint();
         }
     }
 }
