8c5a0949d89dc2dc9d22b4c71a1142be127a8a1e
Mike <mikebulte@roc-dev.com>
2017-03-17 09:29:20 +0100

    +1 bleek niet nodig te zijn

--- src/h14multimedia/Opdr14dot1KaartDelen.java
+++ src/h14multimedia/Opdr14dot1KaartDelen.java
@@ -43,8 +43,8 @@ public class Opdr14dot1KaartDelen extends Applet {
     private class KnopActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            kaartKleurString = kaartKleurArray[(int) (Math.random() * (kaartKleurArray.length + 1))];
-            kaartNummerString = kaartNummerArray[(int) (Math.random() * (kaartNummerArray.length + 1))];
+            kaartKleurString = kaartKleurArray[(int) (Math.random() * (kaartKleurArray.length))];
+            kaartNummerString = kaartNummerArray[(int) (Math.random() * (kaartNummerArray.length))];
             repaint();
         }
     }
