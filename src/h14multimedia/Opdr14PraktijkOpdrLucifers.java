7e54524036c9e59fe0da2d785e389d4851db7cca
Mike <mikebulte@roc-dev.com>
2017-03-29 13:51:02 +0200

    Het spel werkt maar er zit nog geen end bij

--- src/h14multimedia/Opdr14PraktijkOpdrLucifers.java
+++ src/h14multimedia/Opdr14PraktijkOpdrLucifers.java
@@ -13,7 +13,7 @@ public class Opdr14PraktijkOpdrLucifers extends Applet {
 
     private int luciferAantal;
     private int playerAantal;
-    private boolean turnBoolean;
+    private int computerAantal;
 
     private String statusString;
 
@@ -41,8 +41,7 @@ public class Opdr14PraktijkOpdrLucifers extends Applet {
     public void paint(Graphics g) {
         super.paint(g);
 
-        int x = 0,
-                y = 50;
+        int x = 0, y = 50;
 
         g.drawString("" + statusString, 100, 25);
         for (int i = 0; i < luciferAantal; i++) {
@@ -56,22 +55,59 @@ public class Opdr14PraktijkOpdrLucifers extends Applet {
 
     }
 
+    private void PlayerTurnUitwerking() {
+        playerAantal = Integer.parseInt(tekstVeld.getText());
+        if (playerAantal >= 1 && playerAantal <= 3) {
+            luciferAantal = luciferAantal - playerAantal;
+            statusString = "Er zijn " + luciferAantal + " lucifers over.";
+        } else {
+            statusString = "Dit is geen geldige invoer!";
+        }
+    }
+
+    private void ComputerTurnUitwerking() {
+
+        //Bepaalt hoeveel lucifers er worden weggehaalt
+        switch (luciferAantal) {
+            case 2:
+            case 6:
+            case 10:
+            case 14:
+            case 18:
+            case 22:
+                computerAantal = 1;
+                break;
+            case 3:
+            case 7:
+            case 11:
+            case 15:
+            case 19:
+                computerAantal = 2;
+                break;
+            case 4:
+            case 8:
+            case 12:
+            case 16:
+            case 20:
+                computerAantal = 3;
+                break;
+            default:
+                computerAantal = 0;
+                break;
+        }
+
+        luciferAantal = luciferAantal - computerAantal;
+        statusString = "Er zijn " + luciferAantal + " lucifers over.";
+
+    }
+
     private class TekstVeldActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
 
-            //Haal dit later uit de actionlistener en naar een losse methode
-            if (turnBoolean) {
-                playerAantal = Integer.parseInt(tekstVeld.getText());
-                if (playerAantal >= 1 && playerAantal <= 3) {
-                    luciferAantal = luciferAantal - playerAantal;
-                    statusString = "Er zijn " + luciferAantal + " lucifers over.";
-                } else {
-                    statusString = "Dit is geen geldige invoer!";
-                }
-                turnBoolean = false;
-                repaint();
-            }
+            PlayerTurnUitwerking();
+            ComputerTurnUitwerking();
+            repaint();
         }
     }
 }
