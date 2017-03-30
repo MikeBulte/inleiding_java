e2f82b029820bcdd5a83ca8cff5ba4f6d31138c1
Mike <mikebulte@roc-dev.com>
2017-03-30 09:03:54 +0200

    Werkt ongeveer nu, kan nog wat mooier eruit zien

--- src/h14multimedia/Opdr14PraktijkOpdrLucifers.java
+++ src/h14multimedia/Opdr14PraktijkOpdrLucifers.java
@@ -8,17 +8,13 @@ import java.net.URL;
 
 /**
  * Created by Mike on 3/23/2017.
+ * Deze applet bevat een onwinbaar spel met de computer.
  */
 public class Opdr14PraktijkOpdrLucifers extends Applet {
 
     private int luciferAantal;
-    private int playerAantal;
-    private int computerAantal;
-
     private String statusString;
-
     private TextField tekstVeld = new TextField("", 4);
-
     private Image luciferImage;
 
     @Override
@@ -43,7 +39,8 @@ public class Opdr14PraktijkOpdrLucifers extends Applet {
 
         int x = 0, y = 50;
 
-        g.drawString("" + statusString, 100, 25);
+        //Tekent de lucifers op scherm
+        g.drawString("" + statusString, 300, 325);
         for (int i = 0; i < luciferAantal; i++) {
             g.drawImage(luciferImage, x, y, this);
             x += luciferImage.getWidth(this);
@@ -52,11 +49,14 @@ public class Opdr14PraktijkOpdrLucifers extends Applet {
                 y += luciferImage.getHeight(this);
             }
         }
+        if (luciferAantal <= 0) {
+            g.drawString("Je hebt verloren!", 300, 350);
+        }
 
     }
 
     private void PlayerTurnUitwerking() {
-        playerAantal = Integer.parseInt(tekstVeld.getText());
+        int playerAantal = Integer.parseInt(tekstVeld.getText());
         if (playerAantal >= 1 && playerAantal <= 3) {
             luciferAantal = luciferAantal - playerAantal;
             statusString = "Er zijn " + luciferAantal + " lucifers over.";
@@ -68,35 +68,37 @@ public class Opdr14PraktijkOpdrLucifers extends Applet {
     private void ComputerTurnUitwerking() {
 
         //Bepaalt hoeveel lucifers er worden weggehaalt
-        switch (luciferAantal) {
-            case 2:
-            case 6:
-            case 10:
-            case 14:
-            case 18:
-            case 22:
-                computerAantal = 1;
-                break;
-            case 3:
-            case 7:
-            case 11:
-            case 15:
-            case 19:
-                computerAantal = 2;
-                break;
-            case 4:
-            case 8:
-            case 12:
-            case 16:
-            case 20:
-                computerAantal = 3;
-                break;
-            default:
-                computerAantal = 0;
-                break;
+        if (luciferAantal >= 1) {
+            int computerAantal;
+            switch (luciferAantal) {
+                case 2:
+                case 6:
+                case 10:
+                case 14:
+                case 18:
+                case 22:
+                    computerAantal = 1;
+                    break;
+                case 3:
+                case 7:
+                case 11:
+                case 15:
+                case 19:
+                    computerAantal = 2;
+                    break;
+                case 4:
+                case 8:
+                case 12:
+                case 16:
+                case 20:
+                    computerAantal = 3;
+                    break;
+                default:
+                    computerAantal = 1;
+                    break;
+            }
+            luciferAantal = luciferAantal - computerAantal;
         }
-
-        luciferAantal = luciferAantal - computerAantal;
         statusString = "Er zijn " + luciferAantal + " lucifers over.";
 
     }
@@ -105,7 +107,9 @@ public class Opdr14PraktijkOpdrLucifers extends Applet {
         @Override
         public void actionPerformed(ActionEvent e) {
 
-            PlayerTurnUitwerking();
+            if (luciferAantal >= 1) {
+                PlayerTurnUitwerking();
+            }
             ComputerTurnUitwerking();
             repaint();
         }
