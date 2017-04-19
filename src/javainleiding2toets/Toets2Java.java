04d383567d3bc71f8502564027a360fb78c920e3
Mike <mikebulte@roc-dev.com>
2017-04-19 10:55:55 +0200

    Slots roll random.nextint now, not sure if possible to get 3 in a row

--- src/javainleiding2toets/Toets2Java.java
+++ src/javainleiding2toets/Toets2Java.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
+import java.util.Random;
 
 /**
  * Created by Mike on 4/19/2017.
@@ -14,11 +15,10 @@ public class Toets2Java extends Applet {
     private int creditPlayer = 10;
     private String creditString;
 
-    private boolean playBoolean;
+    private boolean playBoolean = false;
 
     private URL url;
-    private Image slotImage1, slotImage2, slotImage3;
-    private String slotString;
+    private Image slotImage[] = new Image[3];
 
     @Override
     public void init() {
@@ -33,24 +33,39 @@ public class Toets2Java extends Applet {
         creditBuyButton.addActionListener(new CreditBuyButtonActionListener());
 
         url = Toets2Java.class.getResource("/images/");
-
+        setSize(500, 400);
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
 
-        if (!playBoolean) {
-            slotImage1 = getImage(url, "fruit_" + (Math.random() * 20));
-        } else {
+        String fruitString;
+        int fruitSlotX = 130;
 
+        for (int i = 0; i < 3; i++) {
+            fruitString = randomFruitSlot();
+            slotImage[i] = getImage(url, "" + fruitString);
+            g.drawImage(slotImage[i], fruitSlotX, 100, 70, 165, this);
+            fruitSlotX += 70;
         }
+        if (slotImage[0] == slotImage[1] && slotImage[1] == slotImage[2]) {
+            g.drawString("3 slots zijn gelijk", 100, 280);
+        } else if (slotImage[0] == slotImage[1] || slotImage[0] == slotImage[2] || slotImage[1] == slotImage[2]) {
+            g.drawString("2 slots zijn gelijk!", 100, 300);
+        }
+    }
 
+    private String randomFruitSlot() {
+        Random randomNumber = new Random();
+        return "fruit_" + (randomNumber.nextInt(20) + 1) + ".jpg";
     }
 
     private class PlayButtonActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+
+            repaint();
 
         }
     }
