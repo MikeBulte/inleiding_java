4fc8227c6935e62571867199142b6e6db3f53146
Mike <mikebulte@roc-dev.com>
2017-05-04 13:17:50 +0200

    De slotmachine werkt met geluid

--- src/javainleiding2toets/Toets2Java.java
+++ src/javainleiding2toets/Toets2Java.java
@@ -1,6 +1,7 @@
 package javainleiding2toets;
 
 import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,20 +10,22 @@ import java.util.Random;
 
 /**
  * Created by Mike on 4/19/2017.
+ * Deze applet maakt een speelbare slot machine.
  */
 public class Toets2Java extends Applet {
 
     private int creditPlayer = 10;
-    private String winString;
+    //Alle slots worden uitgekozen en vergeleken met deze Array.
+    private int slotIntCurrentArray[] = new int[3];
 
-    private boolean firstPlayBoolean = false;
-    private boolean winBoolean = false;
+    private AudioClip purchaseCreditSound, playSound, twoIdenticalSound, threeIdenticalSound;
 
-    private Button playButton;
-    private Button creditBuyButton;
+    private Button playButton, creditBuyButton;
 
-    private URL url;
-    private Image slotImage[] = new Image[3];
+    private Image slotArray[] = new Image[20];
+    private Image slotCurrentArray[] = new Image[3];
+
+    private String winString = "";
 
     @Override
     public void init() {
@@ -36,32 +39,47 @@ public class Toets2Java extends Applet {
         add(creditBuyButton);
         creditBuyButton.addActionListener(new CreditBuyButtonActionListener());
 
-        url = Toets2Java.class.getResource("/images/");
-        setSize(500, 400);
+        URL imageURL = Toets2Java.class.getResource("/images/");
+        URL audioURL = Toets2Java.class.getResource("/sound/");
+        //Laad alle images in een array zodat het steeds niet van de schijf hoeft te laden bij iedere spin
+        for (int i = 0; i < slotArray.length; i++) {
+            slotArray[i] = getImage(imageURL, "fruit_" + i + ".jpg");
+        }
+
+        playSound = getAudioClip(audioURL, "playslots.wav");
+        purchaseCreditSound = getAudioClip(audioURL, "creditpurchase.wav");
+        twoIdenticalSound = getAudioClip(audioURL, "smallwin.wav");
+        threeIdenticalSound = getAudioClip(audioURL, "bigwin.wav");
+
+        setSize(475, 400);
+        checkCredit();
+        randomSlots();
     }
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
 
-        String fruitString;
         int fruitSlotX = 130; //Controleert ook de plaats van de tekst onder de slots.
+
+        g.drawString("" + winString, fruitSlotX, 300);
         g.drawString("Credit over: " + creditPlayer, fruitSlotX, 280);
 
-        if (firstPlayBoolean) {
-            if (winBoolean) {
-                g.drawString("" + winString, fruitSlotX, 300);
-            }
-        }
-
         for (int i = 0; i < 3; i++) {
-            fruitString = randomFruitSlot();
-            slotImage[i] = getImage(url, "" + fruitString);
-            g.drawImage(slotImage[i], fruitSlotX, 100, 70, 165, this);
+            g.drawImage(slotCurrentArray[i], fruitSlotX, 100, 70, 165, this);
             fruitSlotX += 70;
         }
+    }
 
-        if (creditPlayer != 0) {
+    private void randomSlots() {
+        for (int i = 0; i < slotIntCurrentArray.length; i++) {
+            Random randomNumber = new Random();
+            slotIntCurrentArray[i] = randomNumber.nextInt(19) + 1;
+            slotCurrentArray[i] = slotArray[slotIntCurrentArray[i]];
+        }
+    }
+
+    private void checkCredit() {
+        if (creditPlayer > 0) {
             playButton.setEnabled(true);
             creditBuyButton.setEnabled(false);
         } else {
@@ -70,28 +88,27 @@ public class Toets2Java extends Applet {
         }
     }
 
-    private String randomFruitSlot() {
-        Random randomNumber = new Random();
-        return "fruit_" + (randomNumber.nextInt(20) + 1) + ".jpg";
-    }
-
     private class PlayButtonActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            firstPlayBoolean = true;
             creditPlayer--;
-            if (slotImage[0] == slotImage[1] && slotImage[1] == slotImage[2]) {
-                creditPlayer += 20;
-                winBoolean = true;
-                winString = "Je hebt 3 dezelfde, je 20 credit gewonnen";
-            } else if (slotImage[0] == slotImage[1] || slotImage[0] == slotImage[2] || slotImage[1] == slotImage[2]) {
-                creditPlayer += 4;
-                winBoolean = true;
-                winString = "Je hebt 2 dezelfde, je 4 credit gewonnen";
-            } else {
-                winBoolean = false;
-            }
+
+            randomSlots();
             repaint();
+
+            if (slotIntCurrentArray[0] == slotIntCurrentArray[1] && slotIntCurrentArray[1] == slotIntCurrentArray[2]) {
+                creditPlayer += 20;
+                winString = "Je hebt 3 dezelfde, je hebt 20 credit gewonnen!";
+                threeIdenticalSound.play();
+            } else if (slotIntCurrentArray[0] == slotIntCurrentArray[1] || slotIntCurrentArray[1] == slotIntCurrentArray[2]) {
+                creditPlayer += 4;
+                winString = "Je hebt 2 dezelfde, je hebt 4 credit gewonnen!";
+                twoIdenticalSound.play();
+            } else {
+                playSound.play();
+                winString = "";
+            }
+            checkCredit();
         }
     }
 
@@ -99,8 +116,9 @@ public class Toets2Java extends Applet {
         @Override
         public void actionPerformed(ActionEvent e) {
             creditPlayer += 10;
-            winBoolean = true;
             winString = "Je hebt 10 credit gekocht, veel speelplezier!";
+            purchaseCreditSound.play();
+            checkCredit();
             repaint();
         }
     }
